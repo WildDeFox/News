9364898939b3ef32770d049505f7a5419b8f4789
WildMrDeFold <wildmrdefox@gmail.com>
2023-06-17 18:55:06 +0700

    Домашка 1. Добавлены роуты.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Route::get('/hello/{name}', static function (string $name) {
     return "Hello, $name";
 });
+
+Route::get('/info', function () {
+    return "Информация о проекте";
+});
+
+Route::get('/news/{id_news}', static function (string $id_news){
+    return "Новость № $id_news";
+});
